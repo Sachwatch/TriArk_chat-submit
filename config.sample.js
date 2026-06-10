c79d096bbe1d9761ba81bf5f9f090352ff4fcc4f
sachwatch <sachwatch@gmail.com>
2026-06-10 21:00:21 +0900

    TriArk chat app

---
+++ config.sample.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBmlnNwWY2z71UPo0XqMi1wVVArI43GEUI",
+  authDomain: "triark-chat.firebaseapp.com",
+  projectId: "triark-chat",
+  storageBucket: "triark-chat.firebasestorage.app",
+  messagingSenderId: "154559933620",
+  appId: "1:154559933620:web:bd3b26ac162b3a69773be1"
+};
